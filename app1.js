9b8fdb84090314ca0ead6f65cc451e73b0257ad6
AKSHA SHELAT <108681573+aksha22@users.noreply.github.com>
2022-08-05 14:32:47 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app =express();
+
+app.get('/http://127.0.0.1:3000/', function(req, res){
+    res.send("Hello World");
+	
+	
+	
+});
+app.listen(3000);res.send("Hello World");
+
+
+
+app.listen(3000);
---
+++ route_ex_10.js
@@ -0,0 +1,4 @@
+var express = require('Express');
+var app = express();
+var student = require('./student.js');
+//both route_ex_10.js and student.js should be in same directory
